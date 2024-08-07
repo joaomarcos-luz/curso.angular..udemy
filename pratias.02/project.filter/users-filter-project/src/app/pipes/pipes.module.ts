2318bc9af74c33144869fdbbc17335a8278cc8c6
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-08-06 22:17:19 -0300

    status

--- pratias.02/project.filter/users-filter-project/src/app/pipes/pipes.module.ts
+++ pratias.02/project.filter/users-filter-project/src/app/pipes/pipes.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from "@angular/core";
 import { PhonePipe } from './phone.pipe';
 import { CommonModule } from "@angular/common";
 import { AddressPipe } from './address.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule ({
     declarations: [
         PhonePipe,
-        AddressPipe
+        AddressPipe,
+        StatusPipe
   ],
   imports: [
         CommonModule
   ],
     exports: [
         PhonePipe,
-        AddressPipe
+        AddressPipe,
+        StatusPipe
     ],
 })
 
