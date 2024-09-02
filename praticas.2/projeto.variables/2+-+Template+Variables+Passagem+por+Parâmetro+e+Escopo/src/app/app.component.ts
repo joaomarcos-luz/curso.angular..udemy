8b2c61b6d658f810a79ec3113eab2609b7135c31
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-09-02 10:01:55 -0300

    projeto variables

---
+++ praticas.2/projeto.variables/2+-+Template+Variables+Passagem+por+Parâmetro+e+Escopo/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  // clicou(input: HTMLInputElement) {
+  //   console.log(input.value);
+
+  //   input.value = 'Atualizado';
+  // }
+}
+
+
+// const funcaoPai = () => {
+//   let pai = 'pai';
+
+//   const funcaoFilha = () => {
+//     let filha = 'filha';
+
+//     pai = 'teste';
+
+//     const funcaoNeto = () => {
+//       filha = 'teste2';
+//       pai = 'teste';
+//     }
+//   }
+// }
